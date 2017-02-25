23d4a9393114336503d9d7a30c730f3dcbe1f286
Claudio Degioanni <claudiodegio@gmail.com>
2017-02-25 12:16:12 +0100

    Add db4 for test on join tables

---
+++ app/src/main/java/com/claudiodegio/dbsync/sample/db4/InsertArticleActivity.java
@@ -0,0 +1,106 @@
+package com.claudiodegio.dbsync.sample.db4;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.claudiodegio.dbsync.sample.BaseActivity;
+import com.claudiodegio.dbsync.sample.DbSyncApplication;
+import com.claudiodegio.dbsync.sample.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class InsertArticleActivity extends BaseActivity {
+
+    @BindView(R.id.etName)
+    EditText mETName;
+
+    @BindView(R.id.btUpdate)
+    Button btUpdate;
+
+    @BindView(R.id.btInsert)
+    Button btInsert;
+
+    @BindView(R.id.cbNull)
+    CheckBox mCheckBoxNull;
+    private long mId = -1;
+
+    SQLiteDatabase mDB;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_name);
+        app = (DbSyncApplication) getApplication();
+        mDB = app.db2OpenHelper.getWritableDatabase();
+        ButterKnife.bind(this);
+
+        mId = getIntent().getLongExtra("ID", -1);
+
+    }
+
+
+    @OnClick(R.id.btInsert)
+    public void insertName(){
+
+        ContentValues contentValues = new ContentValues();
+
+        if (mCheckBoxNull.isChecked()) {
+            contentValues.put("NAME", (String)null);
+        } else {
+            contentValues.put("NAME", mETName.getEditableText().toString());
+        }
+        contentValues.putNull("SEND_TIME");
+
+        mDB.insert("name", null, contentValues);
+
+        finish();
+    }
+
+    @OnClick(R.id.btUpdate)
+    public void updateName(){
+
+        ContentValues contentValues = new ContentValues();
+
+        if (mCheckBoxNull.isChecked()) {
+            contentValues.put("NAME", (String)null);
+        } else {
+            contentValues.put("NAME", mETName.getEditableText().toString());
+        }
+        contentValues.putNull("SEND_TIME");
+
+        mDB.update("name", contentValues, "_id = ? ", new String[] {Long.toString(mId)});
+
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadById();
+    }
+
+    public void loadById() {
+        Cursor cur;
+
+        if (mId != -1) {
+            cur = mDB.query("name", null, "_id = ?", new String[] {Long.toString(mId)}, null, null, null);
+            cur.moveToFirst();
+            mETName.setText(cur.getString(1));
+            cur.close();
+            btInsert.setVisibility(View.GONE);
+            btUpdate.setVisibility(View.VISIBLE);
+        } else {
+            btInsert.setVisibility(View.VISIBLE);
+            btUpdate.setVisibility(View.GONE);
+        }
+    }
+}
---
+++ app/src/main/java/com/claudiodegio/dbsync/sample/db4/MainDb4Activity.java
@@ -0,0 +1,68 @@
+package com.claudiodegio.dbsync.sample.db4;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.claudiodegio.dbsync.CloudProvider;
+import com.claudiodegio.dbsync.DBSync;
+import com.claudiodegio.dbsync.GDriveCloudProvider;
+import com.claudiodegio.dbsync.TableToSync;
+import com.claudiodegio.dbsync.sample.BaseMainDbActivity;
+import com.claudiodegio.dbsync.sample.R;
+import com.claudiodegio.dbsync.sample.db2.CategoriesActivity;
+import com.claudiodegio.dbsync.sample.db2.NamesActivity;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+
+import butterknife.OnClick;
+
+public class MainDb4Activity extends BaseMainDbActivity  {
+
+    private final static String TAG = "MainDb2Activity";
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main_db2);
+        super.onCreate(savedInstanceState);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        CloudProvider gDriveProvider = new GDriveCloudProvider.Builder(this.getBaseContext())
+                .setSyncFileByDriveId(mDriveId)
+                .setGoogleApiClient(mGoogleApiClient)
+                .build();
+
+        dbSync = new DBSync.Builder(this.getBaseContext())
+                .setCloudProvider(gDriveProvider)
+                .setSQLiteDatabase(app.db2OpenHelper.getWritableDatabase())
+                .setDataBaseName(app.db2OpenHelper.getDatabaseName())
+                .addTable(new TableToSync.Builder("name").build())
+                .addTable(new TableToSync.Builder("category").build())
+                .setSchemaVersion(1)
+                .build();
+   }
+
+    @Override
+    public void onPostSync() {
+    }
+
+
+    @OnClick(R.id.btName)
+    public void goToNames(){
+        startActivity(new Intent(this, NamesActivity.class));
+    }
+
+
+    @OnClick(R.id.btCat)
+    public void goToCats(){
+        startActivity(new Intent(this, CategoriesActivity.class));
+    }
+}
